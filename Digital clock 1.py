2a212222bb5111e0dbe9f2e2ad56d3c9d33b6fda
Anu-004 <138310483+Anu-004@users.noreply.github.com>
2024-03-20 12:59:11 +0530

    Add files via upload

---
+++ Digital clock 1.py
@@ -0,0 +1,17 @@
+from tkinter import * 
+import tkinter as t
+import datetime as d
+rt=t.Tk()
+rt.title("DIGITAL CLOCK")
+rt.geometry("500x500")  #SCREENSIZE
+rt.configure(bg="black")
+def now():
+    tym=(d.datetime.now())
+    a=tym.strftime("%I:%M:%S %p")  #I [12 hr], M [60 minute], S [60 seconds], p [AM/PM]
+    lbl=Label(rt, text=a,bg="grey",fg="black")  #TO DISPLAY OUTPUT
+    lbl.pack()
+    
+
+btn=t.Button(rt,text='Time',width=20,command=now) #BTN SYNTAX
+btn.pack()
+rt.mainloop()
